cdcb4d7697267c9efedf59a103f0dab98a660a84
Balz Lenzi <34675451+EPOCH2017blockchain@users.noreply.github.com>
2017-12-28 18:14:34 +0800

    Add files via upload

---
+++ epoch.UnitTests/TestTransaction.cs
@@ -0,0 +1,20 @@
+﻿using epo.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace epo.UnitTests
+{
+    public class TestTransaction : Transaction
+    {
+        public TestTransaction(UInt256 assetId, TransactionType type, UInt160 scriptHash) : base(type)
+        {
+            TransactionOutput transVal = new TransactionOutput();
+            transVal.Value = Fixed8.FromDecimal(50);
+            transVal.AssetId = assetId;
+            transVal.ScriptHash = scriptHash;
+            base.Outputs = new TransactionOutput[1] { transVal };
+        }
+    }
+}
+t
